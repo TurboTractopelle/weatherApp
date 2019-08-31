085bd2c342f09bc9ce82b4fa933b7771c47942d7
neroark <ns_ark@hotmail.com>
2019-08-31 23:16:14 +0200

    feat: setup basic Customers

---
+++ back/src/controller/customerRoutes.js
@@ -0,0 +1,12 @@
+const Customer = require("../db/Customer");
+
+function setCustomerRoutes(server) {
+  server.get("/customers", getCustomers);
+
+  function getCustomers(req, res, next) {
+    res.send("customers");
+    next();
+  }
+}
+
+module.exports = setCustomerRoutes;
--- back/src/server.js
+++ back/src/server.js
@@ -1,23 +1,25 @@
 const restify = require("restify");
 const villeRoutes = require("./controller/villeRoutes");
+const customerRoutes = require("./controller/customerRoutes");
 
 /**
  * @param  {String} name="weather"
  */
 function setUpServer(name = "weather") {
-	const server = restify.createServer({ name });
+  const server = restify.createServer({ name });
 
-	server.pre(restify.plugins.pre.dedupeSlashes());
-	server.pre(restify.plugins.pre.sanitizePath());
-	server.pre(restify.plugins.pre.strictQueryParams());
-	server.pre(restify.plugins.pre.userAgentConnection());
+  server.pre(restify.plugins.pre.dedupeSlashes());
+  server.pre(restify.plugins.pre.sanitizePath());
+  server.pre(restify.plugins.pre.strictQueryParams());
+  server.pre(restify.plugins.pre.userAgentConnection());
 
-	server.use(restify.plugins.queryParser({ mapParams: false }));
-	server.use(restify.plugins.jsonBodyParser());
+  server.use(restify.plugins.queryParser({ mapParams: false }));
+  server.use(restify.plugins.jsonBodyParser());
 
-	villeRoutes(server);
+  villeRoutes(server);
+  customerRoutes(server);
 
-	return server;
+  return server;
 }
 
 module.exports = setUpServer;
