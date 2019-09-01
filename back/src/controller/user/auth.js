a413c74f72c36554d856b9ebc6034242a91426e0
neroark <ns_ark@hotmail.com>
2019-09-01 16:08:30 +0200

    feat: User set auth

--- back/src/controller/user/auth.js
+++ back/src/controller/user/auth.js
@@ -3,9 +3,26 @@ const errors = require("restify-errors");
 const bcrypt = require("bcryptjs");
 const util = require("util");
 
-async function auth(req, res, next) {
-  res.send("auth");
-  next();
+async function auth(email, password) {
+  try {
+    const user = await User.findOne({ email });
+
+    if (user == null) {
+      throw new errors.ResourceNotFoundError("no user found with this email");
+    }
+
+    const compareProm = util.promisify(bcrypt.compare);
+    // @ts-ignore
+    const legitPassword = await compareProm(password, user.password);
+
+    if (!legitPassword) {
+      throw new errors.UnauthorizedError("bad password");
+    }
+
+    return true;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 module.exports = auth;
--- back/src/controller/userRoutes.js
+++ back/src/controller/userRoutes.js
@@ -1,11 +1,26 @@
 const register = require("./user/register");
-const auth = require("./user/auth");
+const authenticate = require("./user/auth");
 
 function setupUserRoutes(server) {
   console.log("setting up user routes");
 
   server.post("/register", register);
   server.post("/auth", auth);
+
+  function auth(req, res, next) {
+    const { email, password } = req.body;
+
+    authenticate(email, password)
+      .then(response => {
+        console.log(res);
+        res.send(response);
+        next();
+      })
+      .catch(err => {
+        res.send(404, err.message);
+        next();
+      });
+  }
 
   return server;
 }
