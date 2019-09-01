51429820da89e5562897e34e103c4bb042272e76
neroark <ns_ark@hotmail.com>
2019-09-01 13:54:35 +0200

    feat: Customer read user

--- back/src/controller/customerRoutes.js
+++ back/src/controller/customerRoutes.js
@@ -4,6 +4,8 @@ const errors = require("restify-errors");
 function setCustomerRoutes(server) {
   server.get("/customers", getCustomers);
   server.get("/customers/coffee", getCoffee);
+  server.get("/customers/:name", getCustomerByName);
+  server.post("/customers", postCustomers);
 
   async function getCustomers(req, res, next) {
     const data = await Customer.find({});
@@ -11,8 +13,38 @@ function setCustomerRoutes(server) {
     next();
   }
 
+  async function getCustomerByName(req, res, next) {
+    const { name } = req.params;
+    console.log(name);
+    const data = await Customer.find({ name });
+    res.send(data);
+    next();
+  }
+
+  async function postCustomers(req, res, next) {
+    console.log("posting data");
+
+    // check application type to be json
+    if (!req.is("application/json")) {
+      res.send(new errors.InvalidContentError("Waiting for JSON"));
+      return next();
+    }
+    console.log(req.headers);
+    console.log(req.is("application/json"));
+
+    try {
+      const { name, email } = req.body;
+      const infoCreation = await Customer.create({ name, email });
+      res.send(201, infoCreation);
+      next();
+    } catch (error) {
+      res.send(new errors.InvalidArgumentError(error));
+      next();
+    }
+  }
+
   function getCoffee(req, res, next) {
-    next(new errors.ImATeapotError());
+    next(new errors.ImATeapotError("no caffein"));
   }
 }
 
